10257901e2694bc050e31c3bc0e349e63f848a2c
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-02-07 10:59:21 +0900

    init: 프로젝트 설정

---
+++ src/main/java/com/gogoring/dongoorami/DongooramiApplication.java
@@ -0,0 +1,13 @@
+package com.gogoring.dongoorami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongooramiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongooramiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gogoring/dongoorami/DongooramiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogoring.dongoorami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongooramiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
