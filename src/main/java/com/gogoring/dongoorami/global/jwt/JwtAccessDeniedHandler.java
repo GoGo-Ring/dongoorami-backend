1ebf2fcf565685d661bcdf5f414a55fc5a2eae98
letskuku <yujung9288@naver.com>
2024-02-15 03:49:37 +0900

    feat #1: jwt 인증 예외처리에 사용될 JwtAccessDeniedHandler, JwtAuthenticationEntryPoint 구현

---
+++ src/main/java/com/gogoring/dongoorami/global/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.gogoring.dongoorami.global.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("Forbidden Request : {}", request.getRequestURI());
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
---
+++ src/main/java/com/gogoring/dongoorami/global/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.gogoring.dongoorami.global.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized Request : {}", request.getRequestURI());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
