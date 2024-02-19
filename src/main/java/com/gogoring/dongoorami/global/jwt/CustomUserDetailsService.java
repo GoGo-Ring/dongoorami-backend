65b1f95d834691de11a7a6890ed35c49e13618ac
letskuku <yujung9288@naver.com>
2024-02-19 10:36:21 +0900

    feat #1: MemberNotFoundException 구현 및 적용

--- src/main/java/com/gogoring/dongoorami/global/jwt/CustomUserDetailsService.java
+++ src/main/java/com/gogoring/dongoorami/global/jwt/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.gogoring.dongoorami.global.jwt;
 
 import com.gogoring.dongoorami.member.domain.Member;
+import com.gogoring.dongoorami.member.exception.MemberErrorCode;
+import com.gogoring.dongoorami.member.exception.MemberNotFoundException;
 import com.gogoring.dongoorami.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,12 +13,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByProviderIdAndIsActivatedIsTrue(username)
-                .orElseThrow(() -> new IllegalArgumentException("회원 정보가 존재하지 않습니다."));
+                .orElseThrow(() -> new MemberNotFoundException(MemberErrorCode.MEMBER_NOT_FOUND));
 
         return new CustomUserDetails(member);
     }
