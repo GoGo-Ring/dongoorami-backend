10fe03f59f4e2f44d792c403bd1555037024bdc8
letskuku <yujung9288@naver.com>
2024-02-20 12:37:44 +0900

    feat #8: 테스트용 embedded redis 추가 및 적용

---
+++ src/test/java/com/gogoring/dongoorami/config/EmbeddedRedisConfig.java
@@ -0,0 +1,33 @@
+package com.gogoring.dongoorami.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() {
+        try {
+            redisServer = RedisServer.builder()
+                    .port(port)
+                    .setting("maxmemory 256M")
+                    .build();
+            redisServer.start();
+        } catch (Exception ignored) {
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop();
+    }
+}
