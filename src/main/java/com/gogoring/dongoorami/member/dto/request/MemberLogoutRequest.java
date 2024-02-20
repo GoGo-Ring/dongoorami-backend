d6bf9e89c4ebf0045fa17ea46c50e4abbe6ca250
letskuku <yujung9288@naver.com>
2024-02-20 22:20:02 +0900

    feat #11: 계층간 로그아웃 정보 이동 위한 dto 구현

---
+++ src/main/java/com/gogoring/dongoorami/member/dto/request/MemberLogoutRequest.java
@@ -0,0 +1,14 @@
+package com.gogoring.dongoorami.member.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class MemberLogoutRequest {
+
+    @NotBlank(message = "accessToken은 공백일 수 없습니다.")
+    private String accessToken;
+
+    @NotBlank(message = "refreshToken은 공백일 수 없습니다.")
+    private String refreshToken;
+}
