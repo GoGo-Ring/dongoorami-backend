c990661549254e3aecbf03c2a040f930a9b52576
letskuku <yujung9288@naver.com>
2024-02-21 10:14:40 +0900

    test #11: 테스트 환경에서 CustomUserDetails 가져오기 위한 WithCustomMockUser 구현

---
+++ src/test/java/com/gogoring/dongoorami/global/customMockUser/WithCustomMockUser.java
@@ -0,0 +1,14 @@
+package com.gogoring.dongoorami.global.customMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithCustomMockUserSecurityContextFactory.class)
+public @interface WithCustomMockUser {
+    String name() default "김뫄뫄";
+    String profileImage() default "image.png";
+    String provider() default "kakao";
+    String providerId() default "alsjkghlaskdjgh";
+}
