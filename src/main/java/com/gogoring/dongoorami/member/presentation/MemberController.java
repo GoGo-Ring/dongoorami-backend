fced1ef9fbf41c87d87ee9d2d0512c751d9a3601
letskuku <yujung9288@naver.com>
2024-02-23 13:27:38 +0900

    refactor #15: 프로필 이미지 수정 patchMapping에서 postMapping으로 수정

--- src/main/java/com/gogoring/dongoorami/member/presentation/MemberController.java
+++ src/main/java/com/gogoring/dongoorami/member/presentation/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -46,7 +47,7 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/members/profile-image")
+    @PostMapping("/members/profile-image")
     public ResponseEntity<MemberUpdateProfileImageResponse> updateProfileImage(
             @RequestPart("image") MultipartFile multipartFile,
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
