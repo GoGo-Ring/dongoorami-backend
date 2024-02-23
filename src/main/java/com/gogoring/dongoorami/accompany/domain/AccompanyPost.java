e90f9228580c3bc6d4bf743e678691d1eca87d3b
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-02-23 13:00:28 +0900

    feat #9: AccompanyPost, AccompanyPostRepository 생성

---
+++ src/main/java/com/gogoring/dongoorami/accompany/domain/AccompanyPost.java
@@ -0,0 +1,70 @@
+package com.gogoring.dongoorami.accompany.domain;
+
+import com.gogoring.dongoorami.global.common.BaseEntity;
+import com.gogoring.dongoorami.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class AccompanyPost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    private String title;
+
+    private String concertName;
+
+    private String concertPlace;
+
+    private String region;
+
+    private Long startAge;
+
+    private Long endAge;
+
+    private Long totalPeople;
+
+    private String gender;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private String content;
+
+    private String image;
+
+    @Builder
+    public AccompanyPost(Member member, String title, String concertName, String concertPlace,
+            String region, Long startAge, Long endAge, Long totalPeople, String gender,
+            LocalDate startDate, LocalDate endDate, String content) {
+        this.member = member;
+        this.title = title;
+        this.concertName = concertName;
+        this.concertPlace = concertPlace;
+        this.region = region;
+        this.startAge = startAge;
+        this.endAge = endAge;
+        this.totalPeople = totalPeople;
+        this.gender = gender;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/com/gogoring/dongoorami/accompany/repository/AccompanyPostRepository.java
@@ -0,0 +1,8 @@
+package com.gogoring.dongoorami.accompany.repository;
+
+import com.gogoring.dongoorami.accompany.domain.AccompanyPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccompanyPostRepository extends JpaRepository<AccompanyPost, Long> {
+
+}
