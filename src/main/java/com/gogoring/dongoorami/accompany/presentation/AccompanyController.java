aa709add7654ddd8d989eae1973afb1f6d87ac8a
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-02-25 00:18:00 +0900

    feat #9: 동행 게시글 목록 조회 api 구현

--- src/main/java/com/gogoring/dongoorami/accompany/presentation/AccompanyController.java
+++ src/main/java/com/gogoring/dongoorami/accompany/presentation/AccompanyController.java
@@ -2,14 +2,17 @@ package com.gogoring.dongoorami.accompany.presentation;
 
 import com.gogoring.dongoorami.accompany.application.AccompanyService;
 import com.gogoring.dongoorami.accompany.dto.request.AccompanyPostRequest;
+import com.gogoring.dongoorami.accompany.dto.response.AccompanyPostsResponse;
 import com.gogoring.dongoorami.global.jwt.CustomUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccompanyController {
 
     private final AccompanyService accompanyService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<AccompanyPostsResponse> getAccompanyPosts(
+            @RequestParam(required = false) Long cursorId,
+            @RequestParam(required = false, defaultValue = "10") int size) {
+        return ResponseEntity.ok(accompanyService.getAccompanyPosts(cursorId, size));
+    }
 
     @PostMapping("/posts")
     public ResponseEntity<Void> createAccompanyPost(
