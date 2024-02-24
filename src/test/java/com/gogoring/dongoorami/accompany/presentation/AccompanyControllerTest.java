cb262921e9a8a78e48383661bf1d7a90fbca83b7
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-02-25 00:32:15 +0900

    test #9: 동행 구인 게시글 목록 최초 조회 api 테스트 코드 작성

--- src/test/java/com/gogoring/dongoorami/accompany/presentation/AccompanyControllerTest.java
+++ src/test/java/com/gogoring/dongoorami/accompany/presentation/AccompanyControllerTest.java
@@ -1,21 +1,31 @@
 package com.gogoring.dongoorami.accompany.presentation;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gogoring.dongoorami.accompany.domain.AccompanyPost;
 import com.gogoring.dongoorami.accompany.dto.request.AccompanyPostRequest;
+import com.gogoring.dongoorami.accompany.repository.AccompanyPostRepository;
 import com.gogoring.dongoorami.global.customMockUser.WithCustomMockUser;
 import com.gogoring.dongoorami.global.jwt.TokenProvider;
 import com.gogoring.dongoorami.member.domain.Member;
 import com.gogoring.dongoorami.member.repository.MemberRepository;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +47,9 @@ class AccompanyControllerTest {
     private MemberRepository memberRepository;
 
     @Autowired
+    private AccompanyPostRepository accompanyPostRepository;
+
+    @Autowired
     private TokenProvider tokenProvider;
 
     @Autowired
@@ -44,6 +57,18 @@ class AccompanyControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        accompanyPostRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
+
+    @AfterEach
+    void tearDown() {
+        accompanyPostRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
 
     @Test
     @WithCustomMockUser
@@ -110,5 +135,91 @@ class AccompanyControllerTest {
                         ))
 
                 );
+    }
+
+    @Test
+    @WithCustomMockUser
+    @DisplayName("동행 구인글 목록을 조회할 수 있다. - 최초 요청")
+    void success_getAccompanyPostsFirst() throws Exception {
+        // given
+        Member member = (Member) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+        memberRepository.save(member);
+        String accessToken = tokenProvider.createAccessToken(member.getProviderId(),
+                member.getRoles());
+        String postCnt = "3";
+        accompanyPostRepository.saveAll(createAccompanyPosts(member, 5));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get("/api/v1/accompany/posts")
+                        .header("Authorization", accessToken)
+                        .param("size", postCnt)
+        );
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andDo(document("{ClassName}/getAccompanyPostsFirst",
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(parameterWithName("size").description("요청할 동행 구인글 개수")),
+                        responseFields(
+                                fieldWithPath("hasNext").type(JsonFieldType.BOOLEAN)
+                                        .description("다음 동행 구인글 존재 여부"),
+                                fieldWithPath("accompanyPostInfos").type(JsonFieldType.ARRAY)
+                                        .description("동행 구인글 정보 목록"),
+                                fieldWithPath("accompanyPostInfos[].id").type(JsonFieldType.NUMBER)
+                                        .description("동행 구인글 id"),
+                                fieldWithPath("accompanyPostInfos[].title").type(
+                                                JsonFieldType.STRING)
+                                        .description("제목"),
+                                fieldWithPath("accompanyPostInfos[].writer").type(
+                                                JsonFieldType.STRING)
+                                        .description("작성자"),
+                                fieldWithPath("accompanyPostInfos[].updatedAt").type(
+                                                JsonFieldType.STRING)
+                                        .description("작성 날짜"),
+                                fieldWithPath("accompanyPostInfos[].status").type(
+                                                JsonFieldType.STRING)
+                                        .description("구인 상태"),
+                                fieldWithPath("accompanyPostInfos[].concertName").type(
+                                                JsonFieldType.STRING)
+                                        .description("공연명"),
+                                fieldWithPath("accompanyPostInfos[].viewCount").type(
+                                                JsonFieldType.NUMBER)
+                                        .description("조회수"),
+                                fieldWithPath("accompanyPostInfos[].commentCount").type(
+                                                JsonFieldType.NUMBER)
+                                        .description("댓글수"),
+                                fieldWithPath("accompanyPostInfos[].gender").type(
+                                                JsonFieldType.STRING)
+                                        .description("성별"),
+                                fieldWithPath("accompanyPostInfos[].totalPeople").type(
+                                                JsonFieldType.NUMBER)
+                                        .description("모집 인원 수")
+                        )
+                ));
+    }
+
+    private List<AccompanyPost> createAccompanyPosts(Member member, int size) {
+        List<AccompanyPost> accompanyPosts = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            accompanyPosts.add(AccompanyPost.builder()
+                    .member(member)
+                    .concertName("2024 SG워너비 콘서트 : 우리의 노래")
+                    .concertPlace("KSPO DOME")
+                    .startDate(LocalDate.of(2024, 3, 22))
+                    .endDate(LocalDate.of(2024, 3, 22))
+                    .title("서울 같이 갈 울싼 사람 구합니다~~")
+                    .gender("여")
+                    .region("서울")
+                    .content("같이 올라갈 사람 구해요~")
+                    .startAge(23L)
+                    .endAge(37L)
+                    .totalPeople(2L).build());
+        }
+
+        return accompanyPosts;
     }
 }
