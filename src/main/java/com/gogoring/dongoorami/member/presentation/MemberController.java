0bd35d822d2b367dce084f72e998a79c044f77c5
letskuku <yujung9288@naver.com>
2024-02-27 02:36:36 +0900

    feat #17: 프로필 정보 조회 api 구현

--- src/main/java/com/gogoring/dongoorami/member/presentation/MemberController.java
+++ src/main/java/com/gogoring/dongoorami/member/presentation/MemberController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -63,5 +64,11 @@ public class MemberController {
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return ResponseEntity.ok(memberService.updateMember(memberUpdateRequest,
                 customUserDetails.getId()));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<MemberInfoResponse> getMember(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(memberService.getMember(customUserDetails.getId()));
     }
 }
