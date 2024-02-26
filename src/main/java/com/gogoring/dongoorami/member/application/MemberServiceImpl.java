56205c382725af6ea97e0e8b8564d339bb7fdfca
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-02-26 23:47:18 +0900

    chore #9: 코드 리포맷

--- src/main/java/com/gogoring/dongoorami/member/application/MemberServiceImpl.java
+++ src/main/java/com/gogoring/dongoorami/member/application/MemberServiceImpl.java
@@ -15,7 +15,6 @@ import com.gogoring.dongoorami.member.repository.MemberRepository;
 import com.gogoring.dongoorami.member.repository.TokenRepository;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
