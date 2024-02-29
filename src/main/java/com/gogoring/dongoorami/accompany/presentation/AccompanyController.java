aa312c31b97f0edb6b71389b45c177f6288715fe
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-02-29 11:00:44 +0900

    feat #21: 동행 구인글 댓글 작성 api 구현

--- src/main/java/com/gogoring/dongoorami/accompany/presentation/AccompanyController.java
+++ src/main/java/com/gogoring/dongoorami/accompany/presentation/AccompanyController.java
@@ -1,6 +1,7 @@
 package com.gogoring.dongoorami.accompany.presentation;
 
 import com.gogoring.dongoorami.accompany.application.AccompanyService;
+import com.gogoring.dongoorami.accompany.dto.request.AccompanyCommentRequest;
 import com.gogoring.dongoorami.accompany.dto.request.AccompanyPostRequest;
 import com.gogoring.dongoorami.accompany.dto.response.AccompanyPostResponse;
 import com.gogoring.dongoorami.accompany.dto.response.AccompanyPostsResponse;
@@ -13,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,17 @@ public class AccompanyController {
             @Valid AccompanyPostRequest accompanyPostRequest,
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         Long accompanyPostId = accompanyService.createAccompanyPost(accompanyPostRequest,
+                customUserDetails.getId());
+        return ResponseEntity.created(URI.create("/api/v1/accompany/posts/" + accompanyPostId))
+                .build();
+    }
+
+    @PostMapping("/comments/{accompanyPostId}")
+    public ResponseEntity<Void> createAccompanyPostComment(
+            @PathVariable Long accompanyPostId,
+            @Valid @RequestBody AccompanyCommentRequest accompanyCommentRequest,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        accompanyService.createAccompanyComment(accompanyPostId, accompanyCommentRequest,
                 customUserDetails.getId());
         return ResponseEntity.created(URI.create("/api/v1/accompany/posts/" + accompanyPostId))
                 .build();
