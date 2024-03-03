e3ebfd4ad06d0fcad562ede010895944086d994f
letskuku <yujung9288@naver.com>
2024-03-04 01:14:24 +0900

    feature #24: MemberUpdateRequest 필드 값 수정

--- src/main/java/com/gogoring/dongoorami/member/dto/request/MemberUpdateRequest.java
+++ src/main/java/com/gogoring/dongoorami/member/dto/request/MemberUpdateRequest.java
@@ -1,18 +1,13 @@
 package com.gogoring.dongoorami.member.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import java.time.LocalDate;
 import lombok.Getter;
 
 @Getter
 public class MemberUpdateRequest {
 
-    @NotBlank(message = "성별은 공백일 수 없습니다.")
-    private String gender;
-
-    @NotNull(message = "생년월일은 공백일 수 없습니다.")
-    private LocalDate birthDate;
+    @NotBlank(message = "닉네임은 공백일 수 없습니다.")
+    private String name;
 
     @NotBlank(message = "소개는 공백일 수 없습니다.")
     private String introduction;
