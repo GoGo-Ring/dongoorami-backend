91d75fed5548b77c968dee5f92d9fc98c195f3eb
letskuku <yujung9288@naver.com>
2024-03-04 01:15:08 +0900

    feature #24: 계층간 회원 기본 정보 이동 위한 dto 구현

---
+++ src/main/java/com/gogoring/dongoorami/member/dto/request/MemberSignupRequest.java
@@ -0,0 +1,16 @@
+package com.gogoring.dongoorami.member.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+public class MemberSignupRequest {
+
+    @NotBlank(message = "성별은 공백일 수 없습니다.")
+    private String gender;
+
+    @NotNull(message = "생년월일은 공백일 수 없습니다.")
+    private LocalDate birthDate;
+}
