786b663c3c9ab97b11d91be87b494a41aef1c6e9
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-03-04 16:22:10 +0900

    feat #21: 동행 목적 필드 추가

--- src/main/java/com/gogoring/dongoorami/accompany/domain/AccompanyPost.java
+++ src/main/java/com/gogoring/dongoorami/accompany/domain/AccompanyPost.java
@@ -1,5 +1,7 @@
 package com.gogoring.dongoorami.accompany.domain;
 
+import com.gogoring.dongoorami.accompany.exception.AccompanyErrorCode;
+import com.gogoring.dongoorami.accompany.exception.InvalidAccompanyPurposeTypeException;
 import com.gogoring.dongoorami.global.common.BaseEntity;
 import com.gogoring.dongoorami.member.domain.Member;
 import jakarta.persistence.ElementCollection;
@@ -13,6 +15,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -25,7 +28,7 @@ import lombok.NoArgsConstructor;
 public class AccompanyPost extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
-    private final RecruitmentStatus status = RecruitmentStatus.PROCEEDING;
+    private final RecruitmentStatusType status = RecruitmentStatusType.PROCEEDING;
     @OneToMany(mappedBy = "accompanyPost")
     private final List<AccompanyComment> accompanyComments = new ArrayList<>();
     private Long viewCount = 0L;
@@ -47,11 +50,14 @@ public class AccompanyPost extends BaseEntity {
     private String content;
     @ElementCollection
     private List<String> images;
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private List<AccompanyPurposeType> purposes;
 
     @Builder
     public AccompanyPost(Member member, String title, String concertName, String concertPlace,
             String region, Long startAge, Long endAge, Long totalPeople, String gender,
-            LocalDate startDate, LocalDate endDate, String content, List<String> images) {
+            LocalDate startDate, LocalDate endDate, String content, List<String> images, List<AccompanyPurposeType> purposes) {
         this.member = member;
         this.title = title;
         this.concertName = concertName;
@@ -65,6 +71,7 @@ public class AccompanyPost extends BaseEntity {
         this.endDate = endDate;
         this.content = content;
         this.images = images;
+        this.purposes = purposes;
     }
 
     public void increaseViewCount() {
@@ -89,19 +96,40 @@ public class AccompanyPost extends BaseEntity {
         this.endDate = accompanyPost.endDate;
         this.content = accompanyPost.content;
         this.images = accompanyPost.images;
+        this.purposes = accompanyPost.purposes;
     }
 
-    public enum RecruitmentStatus {
+    public enum RecruitmentStatusType {
         PROCEEDING("모집 중"), COMPLETED("모집 완료");
 
         String name;
 
-        RecruitmentStatus(String name) {
+        RecruitmentStatusType(String name) {
             this.name = name;
         }
 
         public String getName() {
             return name;
+        }
+    }
+
+    public enum AccompanyPurposeType {
+        ENJOY("관람"), ACCOMMODATION("숙박"), TRANSPORTATION("이동");
+
+        String name;
+
+        AccompanyPurposeType(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public static AccompanyPurposeType getValue(String name){
+            return Arrays.stream(AccompanyPurposeType.values()).filter(
+                            accompanyPurposeType -> accompanyPurposeType.getName().equals(name)).findAny()
+                    .orElseThrow(() -> new InvalidAccompanyPurposeTypeException(AccompanyErrorCode.INVALID_ACCOMPANY_PURPOSE_TYPE));
         }
     }
 
