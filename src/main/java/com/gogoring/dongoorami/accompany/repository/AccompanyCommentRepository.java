d1f4bcf20d981c222024e832e6335ce7d4ed0ef9
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-03-11 13:34:51 +0900

    feat #42: AccompanyCommentRepository 활성화된 댓글 가져오는 쿼리 메서드 초가

--- src/main/java/com/gogoring/dongoorami/accompany/repository/AccompanyCommentRepository.java
+++ src/main/java/com/gogoring/dongoorami/accompany/repository/AccompanyCommentRepository.java
@@ -1,8 +1,10 @@
 package com.gogoring.dongoorami.accompany.repository;
 
 import com.gogoring.dongoorami.accompany.domain.AccompanyComment;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccompanyCommentRepository extends JpaRepository<AccompanyComment, Long> {
 
+    Optional<AccompanyComment> findByIdAndIsActivatedIsTrue(Long id);
 }
