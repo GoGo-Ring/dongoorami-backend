8aaf4cc1875c73e104a73c1985276fdcf0ddb0da
letskuku <yujung9288@naver.com>
2024-03-13 00:22:34 +0900

    feat #41: 공연 후기 수정 api 구현

--- src/main/java/com/gogoring/dongoorami/concert/presentation/ConcertController.java
+++ src/main/java/com/gogoring/dongoorami/concert/presentation/ConcertController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,5 +41,14 @@ public class ConcertController {
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return ResponseEntity.ok(concertService.getConcertReviews(concertId, cursorId, size,
                 customUserDetails.getId()));
+    }
+
+    @PatchMapping("/concerts/reviews/{concertReviewId}")
+    public ResponseEntity<Void> updateConcertReview(@PathVariable Long concertReviewId,
+            @Valid @RequestBody ConcertReviewRequest concertReviewRequest,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        concertService.updateConcertReview(concertReviewId, concertReviewRequest,
+                customUserDetails.getId());
+        return ResponseEntity.ok().build();
     }
 }
