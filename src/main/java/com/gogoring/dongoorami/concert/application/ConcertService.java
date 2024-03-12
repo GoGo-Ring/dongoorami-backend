16552505dd58fa196b1113683901b579d6148192
letskuku <yujung9288@naver.com>
2024-03-12 09:41:19 +0900

    feat #41: 공연 후기 목록 조회하는 비즈니스 로직 구현

--- src/main/java/com/gogoring/dongoorami/concert/application/ConcertService.java
+++ src/main/java/com/gogoring/dongoorami/concert/application/ConcertService.java
@@ -1,9 +1,13 @@
 package com.gogoring.dongoorami.concert.application;
 
 import com.gogoring.dongoorami.concert.dto.request.ConcertReviewCreateRequest;
+import com.gogoring.dongoorami.concert.dto.response.ConcertReviewsGetResponse;
 
 public interface ConcertService {
 
     void createConcertReview(Long concertId, ConcertReviewCreateRequest concertReviewCreateRequest,
+            Long memberId);
+
+    ConcertReviewsGetResponse getConcertReviews(Long concertId, Long cursorId, int size,
             Long memberId);
 }
