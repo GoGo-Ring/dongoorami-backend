bee3d05ce7c68216047ce60fea3b55641fa50dbd
letskuku <yujung9288@naver.com>
2024-03-12 16:51:57 +0900

    feat #41: ConcertReviewGetResponse에 작성자 닉네임, 작성날짜 필드 추가

--- src/main/java/com/gogoring/dongoorami/concert/dto/response/ConcertReviewGetResponse.java
+++ src/main/java/com/gogoring/dongoorami/concert/dto/response/ConcertReviewGetResponse.java
@@ -1,7 +1,9 @@
 package com.gogoring.dongoorami.concert.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gogoring.dongoorami.concert.domain.ConcertReview;
 import com.gogoring.dongoorami.member.domain.Member;
+import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,6 +11,8 @@ import lombok.Getter;
 public class ConcertReviewGetResponse {
 
     private final Long id;
+
+    private final String nickname;
 
     private final String title;
 
@@ -18,23 +22,30 @@ public class ConcertReviewGetResponse {
 
     private final Boolean isWriter;
 
+    @JsonFormat(pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+    private final LocalDate updatedAt;
+
     @Builder
-    public ConcertReviewGetResponse(Long id, String title, String content, Integer rating,
-            Boolean isWriter) {
+    public ConcertReviewGetResponse(Long id, String nickname, String title, String content,
+            Integer rating, Boolean isWriter, LocalDate updatedAt) {
         this.id = id;
+        this.nickname = nickname;
         this.title = title;
         this.content = content;
         this.rating = rating;
         this.isWriter = isWriter;
+        this.updatedAt = updatedAt;
     }
 
     public static ConcertReviewGetResponse of(ConcertReview concertReview, Member member) {
         return ConcertReviewGetResponse.builder()
                 .id(concertReview.getId())
+                .nickname(member.getNickname())
                 .title(concertReview.getTitle())
                 .content(concertReview.getContent())
                 .rating(concertReview.getRating())
                 .isWriter(concertReview.getMember().getId().equals(member.getId()))
+                .updatedAt(concertReview.getUpdatedAt().toLocalDate())
                 .build();
     }
 }
