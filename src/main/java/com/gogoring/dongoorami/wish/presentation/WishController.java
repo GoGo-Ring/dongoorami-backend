aeb4605751c61c9f7f77e3a9c24204d9d5ba4cf2
letskuku <yujung9288@naver.com>
2024-03-18 00:38:51 +0900

    feat #62: 찜 정보 저장하는 api 구현

---
+++ src/main/java/com/gogoring/dongoorami/wish/presentation/WishController.java
@@ -0,0 +1,27 @@
+package com.gogoring.dongoorami.wish.presentation;
+
+import com.gogoring.dongoorami.global.jwt.CustomUserDetails;
+import com.gogoring.dongoorami.wish.application.WishService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class WishController {
+
+    private final WishService wishService;
+
+    @PostMapping("/wishes/{accompanyPostId}")
+    public ResponseEntity<Void> createWish(@PathVariable Long accompanyPostId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        wishService.createWish(accompanyPostId, customUserDetails.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
