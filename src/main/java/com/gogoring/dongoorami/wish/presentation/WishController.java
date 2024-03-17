c80eaeea4f278d0879fa282e89389a582f7976cd
letskuku <yujung9288@naver.com>
2024-03-18 01:43:01 +0900

    feat #62: 찜 목록 조회하는 api 구현

--- src/main/java/com/gogoring/dongoorami/wish/presentation/WishController.java
+++ src/main/java/com/gogoring/dongoorami/wish/presentation/WishController.java
@@ -2,14 +2,17 @@ package com.gogoring.dongoorami.wish.presentation;
 
 import com.gogoring.dongoorami.global.jwt.CustomUserDetails;
 import com.gogoring.dongoorami.wish.application.WishService;
+import com.gogoring.dongoorami.wish.dto.response.WishesGetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +34,13 @@ public class WishController {
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         wishService.deleteWish(accompanyPostId, customUserDetails.getId());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/wishes")
+    public ResponseEntity<WishesGetResponse> getWishes(
+            @RequestParam(required = false) Long cursorId,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(wishService.getWishes(cursorId, size, customUserDetails.getId()));
     }
 }
