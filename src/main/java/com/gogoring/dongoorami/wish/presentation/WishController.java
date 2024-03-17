198d843a3ddf338e74c594c98d5f488711363ee4
letskuku <yujung9288@naver.com>
2024-03-18 01:00:08 +0900

    feat #62: 찜 정보 삭제하는 api 구현

--- src/main/java/com/gogoring/dongoorami/wish/presentation/WishController.java
+++ src/main/java/com/gogoring/dongoorami/wish/presentation/WishController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +24,12 @@ public class WishController {
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         wishService.createWish(accompanyPostId, customUserDetails.getId());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/wishes/{accompanyPostId}")
+    public ResponseEntity<Void> deleteWish(@PathVariable Long accompanyPostId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        wishService.deleteWish(accompanyPostId, customUserDetails.getId());
+        return ResponseEntity.ok().build();
     }
 }
