c624fa3acfbdf9d9b611b1a3b089e525f4baf307
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-03-19 14:46:13 +0900

    feat #69: MessageRepository 생성

---
+++ src/main/java/com/gogoring/dongoorami/message/repository/MessageRepository.java
@@ -0,0 +1,8 @@
+package com.gogoring.dongoorami.message.repository;
+
+import com.gogoring.dongoorami.message.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+}
