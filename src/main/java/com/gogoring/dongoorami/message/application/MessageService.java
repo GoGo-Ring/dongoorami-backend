aff47c0e053db4ff3a5fecf1c815830e1e1eab41
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-03-20 14:56:27 +0900

    feat #69: 받은 쪽지 목록 반환 비즈니스 로직 구현

--- src/main/java/com/gogoring/dongoorami/message/application/MessageService.java
+++ src/main/java/com/gogoring/dongoorami/message/application/MessageService.java
@@ -1,8 +1,13 @@
 package com.gogoring.dongoorami.message.application;
 
 import com.gogoring.dongoorami.message.dto.request.MessageRequest;
+import com.gogoring.dongoorami.message.dto.response.MessagesResponse;
+import java.util.List;
 
 public interface MessageService {
 
     Long createMessage(MessageRequest messageRequest, Long currentMemberId);
+
+    MessagesResponse getMessages(Long cursorId, int size, List<Long> receivedPartnerIds,
+            Long currentMemberId);
 }
--- src/main/java/com/gogoring/dongoorami/message/application/MessageServiceImpl.java
+++ src/main/java/com/gogoring/dongoorami/message/application/MessageServiceImpl.java
@@ -4,9 +4,14 @@ import com.gogoring.dongoorami.member.domain.Member;
 import com.gogoring.dongoorami.member.exception.MemberErrorCode;
 import com.gogoring.dongoorami.member.exception.MemberNotFoundException;
 import com.gogoring.dongoorami.member.repository.MemberRepository;
+import com.gogoring.dongoorami.message.domain.Message;
 import com.gogoring.dongoorami.message.dto.request.MessageRequest;
+import com.gogoring.dongoorami.message.dto.response.MessageResponse;
+import com.gogoring.dongoorami.message.dto.response.MessagesResponse;
 import com.gogoring.dongoorami.message.repository.MessageRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,4 +34,23 @@ public class MessageServiceImpl implements MessageService {
 
         return messageRequest.getReceiverId();
     }
+
+    @Override
+    public MessagesResponse getMessages(Long cursorId, int size, List<Long> receivedPartnerIds,
+            Long currentMemberId) {
+        Member member = memberRepository.findByIdAndIsActivatedIsTrue(currentMemberId)
+                .orElseThrow(() -> new MemberNotFoundException(MemberErrorCode.MEMBER_NOT_FOUND));
+        Slice<Message> messages = messageRepository.findLatestMessages(member, cursorId, size,
+                receivedPartnerIds);
+
+        return new MessagesResponse(messages.hasNext(),
+                messages.getContent().stream().map(message -> {
+                    Member partner = message.getSender().equals(member) ? message.getReceiver()
+                            : message.getSender();
+                    boolean hasUnRead = messageRepository.existsBySenderAndReceiverAndIsReadFalseAndIsActivatedIsTrue(
+                            partner, member);
+                    return MessageResponse.of(message, partner, member, hasUnRead);
+                }).toList());
+    }
+
 }
