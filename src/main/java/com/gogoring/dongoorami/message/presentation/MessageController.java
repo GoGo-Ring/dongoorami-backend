5699ac14768c1e885a334fca9e043e0cdbb636bd
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-03-20 14:56:40 +0900

    feat #69: 받은 쪽지 목록 반환 api 구현

--- src/main/java/com/gogoring/dongoorami/message/presentation/MessageController.java
+++ src/main/java/com/gogoring/dongoorami/message/presentation/MessageController.java
@@ -3,14 +3,18 @@ package com.gogoring.dongoorami.message.presentation;
 import com.gogoring.dongoorami.global.jwt.CustomUserDetails;
 import com.gogoring.dongoorami.message.application.MessageService;
 import com.gogoring.dongoorami.message.dto.request.MessageRequest;
+import com.gogoring.dongoorami.message.dto.response.MessagesResponse;
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,6 +32,17 @@ public class MessageController {
                         "/api/v1/messages/" + messageService.createMessage(messageRequest,
                                 customUserDetails.getId())))
                 .build();
+    }
+
+    // 이후 요청에는 partnerId도 보내줘야 함.
+    @GetMapping
+    public ResponseEntity<MessagesResponse> getMessages(
+            @RequestParam(required = false) Long cursorId,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam(required = false) List<Long> receivedPartnerIds,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(messageService.getMessages(cursorId, size, receivedPartnerIds,
+                customUserDetails.getId()));
     }
 
 }
