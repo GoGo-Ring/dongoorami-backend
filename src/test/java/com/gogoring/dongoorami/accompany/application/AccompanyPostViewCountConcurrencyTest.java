1e6695e0c16ed75a579b5356e7748cd74d9d3293
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-04-03 21:27:07 +0900

    test #82: update 쿼리로 조회수 직접 업데이트하는 로직에 대한 테스트 코드 작성

--- src/test/java/com/gogoring/dongoorami/accompany/application/AccompanyPostViewCountConcurrencyTest.java
+++ src/test/java/com/gogoring/dongoorami/accompany/application/AccompanyPostViewCountConcurrencyTest.java
@@ -81,4 +81,39 @@ class AccompanyPostViewCountConcurrencyTest {
         Assertions.assertThat(viewCountAfterRequests - viewCountBeforeRequests)
                 .isNotEqualTo(requestCnt);
     }
+
+    @Test
+    @DisplayName("동시에 여러 조회가 이루어지는 경우, 모든 조회수가 정상적으로 반영된다. - 조회수 증가 시 update 문 사용")
+    void success_updateViewCount_given_update_query() {
+        // given
+        Member member = MemberDataFactory.createMember();
+        memberRepository.save(member);
+        Concert concert = concertRepository.save(ConcertDataFactory.createConcert());
+        int size = 1, requestCnt = 100;
+        AccompanyPost accompanyPost = accompanyPostRepository.saveAll(
+                createAccompanyPosts(member, size, concert)).get(0);
+        Long viewCountBeforeRequests = accompanyPost.getViewCount(), viewCountAfterRequests;
+
+        // when
+        long beforeTime = System.currentTimeMillis();
+        List<CompletableFuture<Void>> getAccompanyPostRequestFutures = IntStream.range(0, 100)
+                .mapToObj(i -> CompletableFuture.runAsync(() ->
+                        accompanyService.getAccompanyPostWithViewCountUpdateQuery(member.getId(),
+                                accompanyPost.getId())
+                ))
+                .toList();
+        CompletableFuture.allOf(
+                getAccompanyPostRequestFutures.toArray(
+                        new CompletableFuture[getAccompanyPostRequestFutures.size()])
+        ).join();
+        viewCountAfterRequests = accompanyPostRepository.findById(accompanyPost.getId()).get()
+                .getViewCount();
+        long afterTime = System.currentTimeMillis();
+
+        // then
+        System.out.println("소요시간(ms): " + (afterTime - beforeTime));
+        Assertions.assertThat(viewCountAfterRequests - viewCountBeforeRequests)
+                .isEqualTo(requestCnt);
+    }
+
 }
